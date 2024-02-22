d5dfa50a116cf1d33fb6a26aa90a9e4c8e64b8d5
nokibh <nokibhowlader2003@gmail.com>
2024-02-23 00:26:44 +0600

    added new

--- script/script.js
+++ script/script.js
@@ -1,21 +1,19 @@
-
 // scroll banner to ticket section
 // document.getElementById('bnrBtn').addEventListener('click', function(event){
 //     const busTicket = document.getElementById('busticket');
 
 //     busTicket.scrollIntoView('busticket');
 // })
-// select the button 
+// select the button
 // let buttons = document.getElementsByClassName('green-btn');
 // let count = 0;
 // let seatLeft = 8;
-
 
 // for(const button of buttons){
 //     button.addEventListener('click', function(){
 //         let select = button.style.backgroundColor = 'limegreen';
 //         count = count + 1
-//         setInnerText('total-seat', count);        
+//         setInnerText('total-seat', count);
 //     })
 // }
 
@@ -35,41 +33,65 @@
 
 // }
 
-
 //_____________________________________________________________________________________________________________________//
 
-let buttons = document.getElementsByClassName('green-btn');
+// let buttons = document.getElementsByClassName('green-btn');
 
-for (const button of buttons) {
-    button.addEventListener('click', function (event) {
-        button.style.backgroundColor = 'green'
-        const seatAdd = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[1].childNodes[5].childNodes[1].childNodes[7].innerText
-        
-        const economy = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[1].childNodes[5].childNodes[1].childNodes[7].childNodes[3].innerText
-        console.log(economy)
+// for (const button of buttons) {
+//     button.addEventListener('click', function (event) {
+//         button.style.backgroundColor = 'green'
+//         const seatAdd = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[1].childNodes[5].childNodes[1].childNodes[7].innerText
 
-        const selectedContainer = document.getElementById('ticket-add');
+//         const economy = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[9].childNodes[1].childNodes[5].childNodes[1].childNodes[7].childNodes[3].innerText
+//         console.log(economy)
 
-        const div = document.createElement('div');
-        
-        const p1 = document.createElement('p');
-        p1.innerText = 2;
-        const p2 = document.createElement('p');
-        p2.innerText = economy;
-        const p3 = document.createElement('p');
-        p3.innerText = 550;
+//         const selectedContainer = document.getElementById('ticket-add');
 
-        div.appendChild(p1);
-        div.appendChild(p2);
-        div.appendChild(p3);
-         selectedContainer.appendChild(div);
-    })
+//         const div = document.createElement('div');
 
+//         const p1 = document.createElement('p');
+//         p1.innerText = 2;
+//         const p2 = document.createElement('p');
+//         p2.innerText = economy;
+//         const p3 = document.createElement('p');
+//         p3.innerText = 550;
+
+//         div.appendChild(p1);
+//         div.appendChild(p2);
+//         div.appendChild(p3);
+//          selectedContainer.appendChild(div);
+//     })
+
+// }
+
+// // innerText
+// function setInnerText(id, value) {
+//     document.getElementById(id).innerText = value;
+
+// }
+
+// seat section
+const allbtn = document.getElementsByClassName('green-btn');
+for (const btn of allbtn) {
+  btn.addEventListener('click', function (event) {
+    console.log(event.target);
+    const selectTicket = document.getElementById('selected-ticket');
+    const div = document.createElement('div');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
+    p1.innerText = seat;
+    p2.innerText = Class;
+    p3.innerText = price;
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    selectTicket.appendChild(div);
+  });
 }
 
-// innerText
-function setInnerText(id, value) {
-    document.getElementById(id).innerText = value;
-
+function getConvertedValue(id) {
+  const price = document.getElementById(id).innerText;
+  const convertedPrice = parseInt(price);
+  return convertedPrice;
 }
-
